0d430a61b9ef2e2b7c35c53ad15b06e4d7840595
JSyndicate <jevwilliamson@gmail.com>
2021-10-10 20:31:33 -0500

    tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,4 +6,31 @@ window.onload=function(){
         item.setAttribute("class", "square");
 
     });
+    
+    const boxes = [];
+    const classX = 'X';
+    const classO = 'O';
+    let yourMove = classO;
+
+    box.forEach(elem => {
+        elem.addEventListener('click', boxClicked)
+    })
+    function boxClicked(e){
+        const space = e.target;
+        space.innerHTML = yourMove;
+        space.classList.add('O');
+        console.log('box click');
+        yourMove = yourMove === classX ? classO: classX;
+    }
+    box.forEach(function(elem, index, list){
+        elem.addEventListener('mouseover', function(e)
+        {
+            e.target.classList.add('hover');
+        });
+
+        elem.addEventListener('mouseout', function(e)
+        {
+            e.target.classList.remove('hover');
+        });
+    });
 }
